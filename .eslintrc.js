b8c25be0bd35b79580550e7bf42e4ec96631e7d1
Aarya Tiwari <aarya.tiwari@godrejcapital.com>
2025-05-28 20:52:18 +0530

    ESLint Changes'

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,15 @@
 module.exports = {
-  root: true,
-  extends: 'airbnb-base',
   env: {
     browser: true,
+    es2021: true,
   },
-  parser: '@babel/eslint-parser',
+  extends: ['eslint:recommended'],
   parserOptions: {
-    allowImportExportEverywhere: true,
+    ecmaVersion: 'latest',
     sourceType: 'module',
-    requireConfigFile: false,
   },
   rules: {
-    'import/extensions': ['error', { js: 'always' }], // require js file extensions in imports
-    'linebreak-style': ['error', 'unix'], // enforce unix linebreaks
-    'no-param-reassign': [2, { props: false }], // allow modifying properties of param
+    indent: ['error', 4], // Enforce 4-space indentation
+    'eol-last': ['error', 'always'], // Require newline at end of file
   },
 };
---
+++ .stylelintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: 'stylelint-config-standard',
+  rules: {
+    indentation: 4, // Enforce 4-space indentation
+  },
+};
