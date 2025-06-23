7045f8960d26b36e4d885b145854c75e5c6cdd17
amey.pashte <amey.pashte@godrejcapital.com>
2025-06-23 21:21:52 +0530

    navbar animation added, unwanted scripts removed

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -2,8 +2,6 @@ import { loadFragment } from '../fragment/fragment.js';
 import { getMetadata } from '../../scripts/aem.js';
 
 export default async function decorate(block) {
-  console.log("FAQ block", block);
-
   const fragmentPath = getMetadata('footer');
   const fragment = await loadFragment(fragmentPath, '/fragments/footer');
 
